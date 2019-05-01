f37cf7d60ea3a5ffaba291ec2da781c826431dcf
Ben Lerchin <ben@lerchin.com>
2019-04-30 19:59:04 -0700

    new phrases and spacing

--- print.py
+++ print.py
@@ -4,9 +4,9 @@ from time import sleep
 
 DEVICE='printout.txt'
 
-WIDTH = 80
+WIDTH = 100
 MARGIN = 7
-COLUMN_OFFSETS = [15, 30, 50, 65]
+COLUMN_OFFSETS = [20, 40, 60, 80]
 PHRASE_DISTANCE = 30
 LPM = 100
 
@@ -19,6 +19,10 @@ def get_binary_char():
     return digits[randint(0, len(digits) - 1)]
 
 row = 0
+phrase = None
+left_align = True
+start = 0
+end = 0
 while True:
     line = ''
     for i in range(0, WIDTH):
@@ -27,18 +31,20 @@ while True:
         else:
             line += ' '
 
-    start = MARGIN
-    end = 80 - MARGIN
-    COLUMN_OFFSETS
-    if row % PHRASE_DISTANCE == 0:
+    if row % PHRASE_DISTANCE == PHRASE_DISTANCE - 1:
         phrase = phrases[randint(0, len(phrases) - 1)]
-        left_align = row % (2 * PHRASE_DISTANCE) == 0
-        if left_align:
-            end = MARGIN + len(phrase)
-        else:
-            start = 80 - MARGIN - len(phrase)
-
+        left_align = (row + 1) % (2 * PHRASE_DISTANCE) == 0
+        start = MARGIN if left_align else WIDTH - MARGIN - len(phrase)
+        end = MARGIN + len(phrase) if left_align else WIDTH - MARGIN
+        line = line[:start] + (' ' * len(phrase)) + line[end:]
+    elif phrase != None and row % PHRASE_DISTANCE == 0:
         line = line[:start] + phrase + line[end:]
+    elif phrase != None and row % PHRASE_DISTANCE == 1:
+        line = line[:start] + (' ' * len(phrase)) + line[end:]
+    else:
+        start = MARGIN
+        end = WIDTH - MARGIN
+        phrase = None
 
     print(line)
     row += 1
